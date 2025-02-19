5458b71cffbd0706cdccb8f29f1fd8cb9ac0e664
heejung-yu <hjung001206@gmail.com>
2025-02-19 15:44:59 +0900

    Edit: 코드 수정

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os, sys
 import read_ami_kipris_data
+import create_ner_data_using_gpt
+import data_preprocessing
 
 if __name__ == "__main__":
     
     read_ami_kipris_data.main()   
+    
+    #TODO: 차후 수정 --------------------------------
+    data_preprocessing.main()  #TODO: 이 함수 수정 해야 됨
+    create_ner_data_using_gpt.main()
+    
+    
     
     
