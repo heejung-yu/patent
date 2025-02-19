3a824c860ce54e43e82714b778994fa9b9e91188
heejung-yu <hjung001206@gmail.com>
2025-02-19 15:52:00 +0900

    Edit: 코드 수정

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     
     read_ami_kipris_data.main()   
     
-    #TODO: 차후 수정 --------------------------------
-    data_preprocessing.main()  #TODO: 이 함수 수정 해야 됨
+    #TODO: 코드 수정이 필요함--------------------------------
+    data_preprocessing.main()  
     create_ner_data_using_gpt.main()
     
     
